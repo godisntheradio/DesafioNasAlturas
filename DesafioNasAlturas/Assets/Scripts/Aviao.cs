b949e6bd3c2853568b36c1501637570051306750
godisntheradio <gabriel_souzadasilva@yahoo.com.br>
2020-03-16 14:32:54 -0300

    first commit

---
+++ DesafioNasAlturas/Assets/Scripts/Aviao.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Aviao : MonoBehaviour
+{
+    [SerializeField]
+    public float Force = 1;
+
+    Rigidbody2D Rigidbody;
+    void Start()
+    {
+        Rigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    
+    void Update()
+    {
+        if (Input.GetButtonDown("Impulse"))
+        {
+            ApplyMovement();
+        }
+    }
+
+    public void ApplyMovement()
+    {
+        Rigidbody.AddForce(Vector2.up * Force, ForceMode2D.Impulse);
+    }
+}
