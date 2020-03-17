d365d9257bf2298ece17cedc8307e0b25eb2e046
godisntheradio <gabriel_souzadasilva@yahoo.com.br>
2020-03-17 13:52:43 -0300

    fim do curso Unity 2D parte 1

--- DesafioNasAlturas/Assets/Scripts/Pontuacao.cs
+++ DesafioNasAlturas/Assets/Scripts/Pontuacao.cs
@@ -8,11 +8,18 @@ public class Pontuacao : MonoBehaviour
     [SerializeField]
     private Text ScreenScore;
     private int Score = 0;
+    AudioSource Audio;
+
+    private void Awake()
+    {
+        Audio = GetComponent<AudioSource>();    
+    }
 
     public void Increment()
     {
         Score++;
         ScreenScore.text = Score.ToString();
+        Audio.Play();
     }
     public void Restart()
     {
