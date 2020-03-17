888e56519c270488b5c60963d61151c65d1d3c1c
godisntheradio <gabriel_souzadasilva@yahoo.com.br>
2020-03-17 14:21:46 -0300

    fim da primeira aula da parte 2

---
+++ DesafioNasAlturas/Assets/Scripts/MaoPiscando.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MaoPiscando : MonoBehaviour
+{
+    void Update()
+    {
+        if (Input.GetButtonDown("Impulse"))
+        {
+            GetComponent<SpriteRenderer>().enabled = false;
+        }
+    }
+}
